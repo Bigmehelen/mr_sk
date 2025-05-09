e25d43863af97a69ec896804fc18f065c4e67cab
Bigmehelen <getfitbyhelena@gmail.com>
2025-05-09 07:43:49 +0100

    chapter_two

---
+++ passwordCheck.py
@@ -0,0 +1,25 @@
+for word in range(0 , 5):
+
+	password = str(input("Enter the password: "))
+	length = len(password)
+
+	if length > 0 and length < 8:
+		print("very weak")
+
+	elif length == 8:
+		print("weak")
+
+	elif length > 8 and length < 16:
+		print("strong")
+
+	elif length > 16:
+		print("very strong")
+
+	else:print("enter a valid password")
+
+
+
+
+
+
+		
---
+++ separate.py
@@ -0,0 +1,7 @@
+num = int(input("Enter a five digit number: "))
+num1 = num // 10000 % 10
+num2 = num // 1000 % 10
+num3 = num // 100 % 10
+num4 = num // 10 % 10
+num5 = num % 10
+print(num1,"   ",num2,"   ",num3,"   ",num4,"   ",num5)
